7157ada2dc8ec90abb893f677d85da095160caf3
digidon-son <e.lepehin93@gmail.com>
2022-04-26 22:14:49 +0300

    lesson53.0.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
-    User.create(user_params)
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    user_params = params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
+
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    else
+      flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации'
+      render :new
+    end
   end
 end
