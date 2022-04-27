cd792a22f8b8f9911879dd0ba079fa6075748415
digidon-son <e.lepehin93@gmail.com>
2022-04-27 18:31:15 +0300

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,17 @@ class User < ApplicationRecord
   before_save :downcase_nickname
 
   validates :email, presence: true, uniqueness: true
-
+  validates_each :email do |record, attr, value|
+    record.errors.add(attr, 'Введен некоректный email') unless value =~ /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  end
+  validates :nickname, uniqueness: true, length: { maximum: 40 }
+  validates_each :nickname do |record, attr, value|
+    unless value =~ /^[A-Za-z0-9_]*$/
+      record.errors.add(attr,
+                        'Никнейм может содержать только латинские буквы, цифры, и знак _')
+    end
+  end
+  
   def downcase_nickname
     nickname.downcase!
   end
