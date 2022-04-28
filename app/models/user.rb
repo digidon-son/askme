cb11685b0619cd9c167bdf91c3515c9911696a5d
digidon-son <e.lepehin93@gmail.com>
2022-04-28 23:20:31 +0300

    add validation for db and edit validation for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,13 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  VALID_NICKNAME_REGEXP = /\A[\w_]*\Z/
   has_secure_password
 
   before_save :downcase_nickname
 
-  validates :email, presence: true, uniqueness: true
-  validates_each :email do |record, attr, value|
-    record.errors.add(attr, 'invalid') unless value =~ /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-  end
-  validates :nickname, uniqueness: true, length: { maximum: 40 }
-  validates_each :nickname do |record, attr, value|
-    unless value =~ /^[A-Za-z0-9_]*$/
-      record.errors.add(attr,
-                        'can only contain Latin letters, numbers, and the _ sign')
-    end
-  end
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEXP }
+
+  validates :nickname, uniqueness: true, length: { maximum: 40 }, format: { with: VALID_NICKNAME_REGEXP }
 
   def downcase_nickname
     nickname.downcase!
