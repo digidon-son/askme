e7f2e0ff71c91c60f6422be324d77b7e3898c7b2
digidon-son <e.lepehin93@gmail.com>
2022-05-01 12:11:29 +0300

    add validates header_color

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEXP =  /\A[\w]+@[\w]+\.[a-z]+/
-  VALID_NICKNAME_REGEXP = /\A[\w]*\Z/
+  VALID_EMAIL_REGEXP =  /\A\w+@\w+\.[a-z]+/
+  VALID_NICKNAME_REGEXP = /\A\w*\Z/
+  VALID_HEAD_COLOR_REGEXP = /\A\#+[0-9]*\Z/
   has_secure_password
 
   before_save :downcase_nickname
@@ -8,6 +9,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEXP }
 
   validates :nickname, uniqueness: true, length: { maximum: 40 }, format: { with: VALID_NICKNAME_REGEXP }
+
+  validates :header_color, length: { maximum: 7 }, format: { with: VALID_HEAD_COLOR_REGEXP }
 
   def downcase_nickname
     nickname.downcase!
