7f447d5abc978d9dec6041c912678a783d589b63
digidon-son <e.lepehin93@gmail.com>
2022-05-04 21:59:48 +0300

    bug fix by Edem

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,18 +2,10 @@ module QuestionsHelper
   def hide_question(question)
     if question.hidden?
       button_to fa_icon('eye-slash'), hide_question_path(question), method: :put, title: 'Вопрос скрыт. Показать?',
-                class: 'mr-sm'
+                                                                    class: 'mr-sm'
     else
       button_to fa_icon('eye'), hide_question_path(question), method: :put, title: 'Вопрос виден. Скрыть?',
-                class: 'mr-sm'
-    end
-  end
-
-  def author
-    if current_user.present?
-      current_user.id
-    else
-      @question.user_id
+                                                              class: 'mr-sm'
     end
   end
 end
--- db/migrate/20220502192545_add_author_to_questions.rb
+++ db/migrate/20220502192545_add_author_to_questions.rb
@@ -1,6 +1,6 @@
 class AddAuthorToQuestions < ActiveRecord::Migration[7.0]
   def change
-    add_column :questions, :author_id, :integer
+    add_column :questions, :author_id, :integer, column_options: { null: true }
     add_index :questions, :author_id
   end
 end
