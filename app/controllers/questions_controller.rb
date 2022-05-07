3046bdfafcd27de529d5b7a7d1b95e40194a41c2
digidon-son <e.lepehin93@gmail.com>
2022-05-07 14:05:46 +0300

    removed @user from create action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,11 +3,10 @@ class QuestionsController < ApplicationController
   before_action :set_question_for_current_user, only: %i[update destroy edit hide]
 
   def create
-    question_params = params.require(:question).permit(:body, :user_id, :author_id)
+    question_params = params.require(:question).permit(:body, :user_id)
 
     @question = Question.new(question_params)
     @question.author = current_user
-    @user = @question.user
 
     if @question.save
       redirect_to user_path(@question.user), notice: 'Новый вопрос создан!'
