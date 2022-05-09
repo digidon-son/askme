402a45a0dec65b84d6a9f0f010a5e6d4de5a6ea4
digidon-son <e.lepehin93@gmail.com>
2022-05-09 19:32:34 +0300

    replaced user_id with user_nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,25 @@
 class User < ApplicationRecord
+  include Gravtastic
+
   VALID_EMAIL_REGEXP = /\A\w+@\w+\.[a-z]+/
   VALID_NICKNAME_REGEXP = /\A\w*\Z/
   VALID_HEAD_COLOR_REGEXP = /\A#\h{3}{1,2}\z/
+
+  has_many :questions, dependent: :destroy
+
   has_secure_password
 
   before_save :downcase_nickname
 
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEXP }
-
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 },
                        format: { with: VALID_NICKNAME_REGEXP }
-
   validates :header_color, length: { minimum: 4, maximum: 7 }, format: { with: VALID_HEAD_COLOR_REGEXP }
 
-  has_many :questions, dependent: :destroy
-
-  include Gravtastic
   gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
 
   def downcase_nickname
-    nickname.downcase!
+    nickname&.downcase!
   end
 
   def to_param
